671ed22718ee36cec212502692e783b34bdbd37f
Jorge Castro <david92_jc@yahoo.es>
2023-04-10 18:14:27 -0500

    Creado template manual de React

--- ejercicio-1/src/components/containers/Context.tsx
+++ ejercicio-1/src/components/containers/Context.tsx
@@ -2,16 +2,9 @@ import React, { createContext, useReducer, ReactNode } from "react";
 import reducer from "../../store/reducers/reducer";
 import { State, Action } from "../../store/types/types";
 
-const initialState: State = {
-    tasks: [],
-    filteredTasks: [],
-};
+const initialState: State = { tasks: [], filteredTasks: [], };
 
-export interface ContextProps {
-    state: State;
-    dispatch: React.Dispatch<Action>;
-    children: ReactNode;
-}
+export interface ContextProps { state: State; dispatch: React.Dispatch<Action>; children: ReactNode; }
 
 export const TaskContext = createContext({} as ContextProps);
 
--- ejercicio-1/src/store/types/types.ts
+++ ejercicio-1/src/store/types/types.ts
@@ -1,16 +1,4 @@
-export interface Task {
-    id: number;
-    title: string;
-    completed: boolean;
-}
-
-export interface State {
-    tasks: Task[];
-    filteredTasks: Task[];
-}
-
-export type Action =
-    | { type: "ADD_TASK"; task: Task }
-    | { type: "DELETE_TASK"; id: number }
-    | { type: "TOGGLE_TASK"; id: number }
-    | { type: "FILTER_TASKS"; keyword: string };
+export interface Task { id: number; title: string; completed: boolean; }
+export interface State { tasks: Task[]; filteredTasks: Task[]; }
+export type Action = { type: "ADD_TASK"; task: Task } | { type: "DELETE_TASK"; id: number }
+    | { type: "TOGGLE_TASK"; id: number } | { type: "FILTER_TASKS"; keyword: string };
