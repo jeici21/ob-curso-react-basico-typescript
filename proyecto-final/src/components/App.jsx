9c4701e643f3f2d032675eabbf4354490e0ef2eb
Jorge Castro <david92_jc@yahoo.es>
2023-04-10 22:41:48 -0500

    Añadida documentación.

---
+++ proyecto-final/src/components/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+/**
+ * Función Anónima para crear un Componente principal
+ * @returns {Component} Componente principal de nuestra aplicación
+ */
+const App = () => {
+    return (
+        <div><h1>Proyecto final</h1></div>
+    );
+};
+
+export default App
